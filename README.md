df1b4b592140584dc742b068c5ca1065dd3cfcbf
glitter2626 <gercommand@gmail.com>
2021-08-02 01:26:37 +0800

    first edition

---
+++ README.md
@@ -0,0 +1,17 @@
+0. 安裝所需套件:
+* pip install numpy
+* pip install scipy
+* pip install pandas
+* pip install opencv-python
+* pip install scikit-image
+* pip install json
+* pip install matplotlib
+1. 執行data_prepare.ipynb處理資料，並輸出COCO格式的json檔在TrainDataset、PublicTestDataset、PrivateTestDataset資料夾中。
+2. 執行train_inference.ipynb訓練模型，訓練權重會儲存在mmocr-main/work-dirs/exp/，推理後的csv檔會儲存在當前目錄。
+3. 模型訓練時的參數與讀取檔案的路徑皆寫在mmocr-main/configs/textdet/maskrcnn/目錄中的public_deform_mask_rcnn_101.py(for public test data推理)與private_deform_mask_rcnn_101.py(for private test data推理)，兩者除了推理檔案的路徑參數不同之外，其他參數皆相同
+4. 工具庫運行架構可參考: https://mmdetection.readthedocs.io/en/latest/ 
+5. 檔案擺放如下:
+
+![](https://i.imgur.com/WWiVdge.png)
+
+  
---
+++ mmocr-main/configs/textdet/maskrcnn/public_deform_mask_rcnn_101.py
@@ -0,0 +1,225 @@
+
+# model settings
+model = dict(
+    type='OCRMaskRCNN',
+    pretrained='open-mmlab://resnext101_64x4d',
+    backbone=dict(
+        type='ResNeXt',
+        depth=101,
+        groups=64,
+        base_width=4,
+        num_stages=4,
+        out_indices=(0, 1, 2, 3),
+        frozen_stages=1,
+        dcn=dict(type='DCN', deform_groups=1, fallback_on_stride=False),
+        stage_with_dcn=(False, True, True, True),
+        norm_cfg=dict(type='BN', requires_grad=True),
+        style='pytorch'),
+    neck=dict(
+        type='FPN',
+        in_channels=[256, 512, 1024, 2048],
+        out_channels=256,
+        num_outs=5),
+    rpn_head=dict(
+        type='RPNHead',
+        in_channels=256,
+        feat_channels=256,
+        anchor_generator=dict(
+            type='AnchorGenerator',
+            scales=[4],
+            ratios=[0.17, 0.44, 1.13, 2.90, 7.46],
+            strides=[4, 8, 16, 32, 64]),
+        bbox_coder=dict(
+            type='DeltaXYWHBBoxCoder',
+            target_means=[.0, .0, .0, .0],
+            target_stds=[1.0, 1.0, 1.0, 1.0]),
+        loss_cls=dict(
+            type='CrossEntropyLoss', use_sigmoid=True, loss_weight=1.0),
+        loss_bbox=dict(type='L1Loss', loss_weight=1.0)),
+    roi_head=dict(
+        type='StandardRoIHead',
+        bbox_roi_extractor=dict(
+            type='SingleRoIExtractor',
+            roi_layer=dict(type='RoIAlign', output_size=7, sampling_ratio=0),
+            out_channels=256,
+            featmap_strides=[4, 8, 16, 32]),
+        bbox_head=dict(
+            type='Shared2FCBBoxHead',
+            in_channels=256,
+            fc_out_channels=1024,
+            roi_feat_size=7,
+            num_classes=1,
+            bbox_coder=dict(
+                type='DeltaXYWHBBoxCoder',
+                target_means=[0., 0., 0., 0.],
+                target_stds=[0.1, 0.1, 0.2, 0.2]),
+            reg_class_agnostic=False,
+            loss_cls=dict(
+                type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0),
+            loss_bbox=dict(type='L1Loss', loss_weight=1.0)),
+        mask_roi_extractor=dict(
+            type='SingleRoIExtractor',
+            roi_layer=dict(type='RoIAlign', output_size=14, sampling_ratio=0),
+            out_channels=256,
+            featmap_strides=[4, 8, 16, 32]),
+        mask_head=dict(
+            type='FCNMaskHead',
+            num_convs=4,
+            in_channels=256,
+            conv_out_channels=256,
+            num_classes=1,
+            loss_mask=dict(
+                type='CrossEntropyLoss', use_mask=True, loss_weight=1.0))),
+
+    # model training and testing settings
+    train_cfg=dict(
+        rpn=dict(
+            assigner=dict(
+                type='MaxIoUAssigner',
+                pos_iou_thr=0.7,
+                neg_iou_thr=0.3,
+                min_pos_iou=0.3,
+                match_low_quality=True,
+                ignore_iof_thr=-1,
+                gpu_assign_thr=50),
+            sampler=dict(
+                type='RandomSampler',
+                num=256,
+                pos_fraction=0.5,
+                neg_pos_ub=-1,
+                add_gt_as_proposals=False),
+            allowed_border=-1,
+            pos_weight=-1,
+            debug=False),
+        rpn_proposal=dict(
+            nms_across_levels=False,
+            nms_pre=2000,
+            nms_post=1000,
+            max_num=1000,
+            nms_thr=0.7,
+            min_bbox_size=0),
+        rcnn=dict(
+            assigner=dict(
+                type='MaxIoUAssigner',
+                pos_iou_thr=0.5,
+                neg_iou_thr=0.5,
+                min_pos_iou=0.5,
+                match_low_quality=True,
+                ignore_iof_thr=-1),
+            sampler=dict(
+                type='OHEMSampler',
+                num=512,
+                pos_fraction=0.25,
+                neg_pos_ub=-1,
+                add_gt_as_proposals=True),
+            mask_size=28,
+            pos_weight=-1,
+            debug=False)),
+    test_cfg=dict(
+        rpn=dict(
+            nms_across_levels=False,
+            nms_pre=1000,
+            nms_post=1000,
+            max_num=1000,
+            nms_thr=0.7,
+            min_bbox_size=0),
+        rcnn=dict(
+            score_thr=0.05,
+            nms=dict(type='nms', iou_threshold=0.5),
+            max_per_img=100,
+            mask_thr_binary=0.65)))
+
+dataset_type = 'IcdarDataset'
+classes = ('text')
+#data_root = '../'
+#val_root = '/content/drive/MyDrive/aicup_ocr/aicup_ocr/'
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+# img_norm_cfg = dict(mean=[0, 0, 0], std=[1, 1, 1], to_rgb=True)
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations', with_bbox=True, with_mask=True),
+    dict(
+        type='ScaleAspectJitter',
+        img_scale=None,
+        keep_ratio=False,
+        resize_type='indep_sample_in_range',
+        scale_range=(640, 2560)),
+    dict(type='RandomFlip', flip_ratio=0.5),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(
+        type='RandomCropInstances',
+        target_size=(640, 640),
+        mask_type='union_all',
+        instance_key='gt_masks'),
+    dict(type='Pad', size_divisor=32),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels', 'gt_masks']),
+]
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='MultiScaleFlipAug',
+        # resize the long size to 1600
+        img_scale=(1280, 1280),
+        flip=False,
+        transforms=[
+            dict(type='Resize', keep_ratio=True),
+            # no flip
+            dict(type='RandomFlip'),
+            dict(type='Normalize', **img_norm_cfg),
+            dict(type='ImageToTensor', keys=['img']),
+            dict(type='Collect', keys=['img']),
+        ])
+]
+data = dict(
+    samples_per_gpu=4,
+    workers_per_gpu=4,
+    train=dict(
+        type=dataset_type,
+        ann_file='TrainDataset/instances_training.json',
+        img_prefix='TrainDataset/img',
+        pipeline=train_pipeline),
+    val=dict(
+        type=dataset_type,
+        # select_first_k=1,
+        ann_file='TrainDataset/instances_training.json',
+        img_prefix='TrainDataset/img',
+        pipeline=test_pipeline),
+    test=dict(
+        type=dataset_type,
+        # select_first_k=1,
+        ann_file='PublicTestDataset/instances_public_test.json',
+        img_prefix='PublicTestDataset/img',
+        pipeline=test_pipeline)
+        
+        )
+evaluation = dict(interval=2, metric='hmean-iou')
+#load_from = '/content/drive/MyDrive/aicup_ocr/mmocr-main/checkpoints/mask_rcnn_r50_fpn_160e_icdar2015_20210219-8eb340a3.pth'
+
+# optimizer
+optimizer = dict(type='SGD', lr=0.005, momentum=0.9, weight_decay=0.0001)
+optimizer_config = dict(grad_clip=None)
+# learning policy
+lr_config = dict(
+    policy='step',
+    warmup='linear',
+    warmup_iters=500,
+    warmup_ratio=0.001,
+    step=[50, 80])
+total_epochs = 100
+
+checkpoint_config = dict(interval=1)
+# yapf:disable
+log_config = dict(
+    interval=5,
+    hooks=[
+        dict(type='TextLoggerHook')
+        # dict(type='TensorboardLoggerHook')
+    ])
+# yapf:enable
+dist_params = dict(backend='nccl')
+log_level = 'INFO'
+load_from = None #'/content/drive/MyDrive/aicup_ocr/mmocr-main/work_dirs/mask_rcnn_rects_16/epoch_2.pth'
+resume_from = None #'mmocr-main/work_dirs/exp9/latest.pth'
+workflow = [('train', 1)]
